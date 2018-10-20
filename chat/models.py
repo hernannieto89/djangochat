34a16d51862f9325021f79da18a151bb080f8f8c
hernan <hernan.nieto89@gmail.com>
2018-10-20 17:06:47 -0300

    Inital commit

---
+++ chat/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class ChatMessage(models.Model):
+    """
+    Model to represent user submitted changed to a resource guide
+    """
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    message = models.TextField(max_length=3000)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        """
+        String to represent the message
+        """
+
+        return self.message
